d783b3383cb53644f609731232e2c54bbfe7285f
wangshibiao <645102170@qq.com>
2019-10-23 16:07:11 +0800

    feat(游戏): 初始化4399游戏爬虫项目
    采用的技术:puppeteer和axios

---
+++ spider.js
@@ -0,0 +1,108 @@
+const puppeteer = require("puppeteer");
+const axios = require("axios");
+const fs = require("fs");
+
+crawGameList();
+
+/**
+ * 爬取游戏列表
+ */
+async function crawGameList(){
+    let gameList = [];
+    let pageCount = 633;
+    // 请求游戏api, 获取游戏列表
+    for (let i = 0; i < pageCount; i++) {
+        // 请求游戏列表api
+        let apiUrl = "http://h.4399.com/data/pc_xyx_new_"+i+".js"
+        let resp = await axios({
+            url: apiUrl
+        });
+
+        if (resp.status != 200){
+            console.log("爬取游戏列表， 请求游戏列表api, 异常")
+            continue;
+        }
+
+        // 组装游戏列表
+        let respData = resp.data;
+        let gameListRet = respData.data;
+        for (let j = 0; j < gameListRet.length; j++) {
+            let element = gameListRet[j];
+            let game = {
+                title: element.title,
+                label: element.category,
+                coverUrl: element.pic,
+                detailUrl: element.pczzylink
+            };
+            gameList.push(game);
+        }
+
+        // 睡眠
+        sleep(1000);
+        console.log("爬取游戏列表, 请求游戏api, 当前进度:"+i+", 总数:"+pageCount);
+    }
+    console.log("爬取游戏列表, 请求游戏api, 完成");
+
+    // 补充游戏url
+    await supplyGameUrl(gameList);
+    console.log("爬取游戏列表, 补充游戏url, 完成");
+
+    // 将爬取结果数据写入文件
+    fs.writeFile('./gameList', JSON.stringify(gameList),  function(err) {
+        if (err) {
+            return console.error(err);
+        }
+
+        console.log("爬取游戏列表, 将爬取结果数据写入文件, 完成");
+    });
+}
+
+/**
+ * 获取游戏的url
+ * @param detailUrl
+ * @returns {Promise<any>}
+ */
+async function getGameUrl(detailUrl) {
+    const browser = await puppeteer.launch({headless: false});
+    let page = await browser.newPage();
+    await page.goto(detailUrl);
+    await page.click(".play .btn")
+    // 取出游戏链接
+    let gameIframeSelector = "iframe#flash22";
+    await page.waitForSelector(gameIframeSelector)
+    const gameUrl = await page.$eval(gameIframeSelector, el => el.src);
+    await browser.close();
+
+    console.log("获取游戏的url, 完成, detailUrl:"+detailUrl+", gameUrl:"+gameUrl);
+    return gameUrl;
+}
+
+/**
+ * 补充游戏url
+ * @param gameList
+ */
+async function supplyGameUrl(gameList) {
+    for (let i = 0; i < gameList.length; i++) {
+        let game = gameList[i];
+        game["link"] = await getGameUrl(game.detailUrl);
+
+        // 睡眠
+        sleep(500);
+        console.log("补充游戏url, 当前进度:"+ i + ", 总数:"+ gameList.length);
+    }
+
+    console.log("补充游戏url, 完成, 总数:"+ gameList.length);
+}
+
+/**
+ * 睡眠
+ * @param time
+ * @returns {Promise<unknown>}
+ */
+function sleep(time = 0) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    })
+};
